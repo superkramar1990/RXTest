53f29434384913d0bc1a1b4f5dec51b5a746d98f
Stanislav <stanislav.kramarenko@btsdigital.kz>
2019-06-29 16:25:49 +0600

    Add xcode project

---
+++ src/RXTextProject/RXTextProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RXTextProject
+//
+//  Created by Stanislav on 29/06/2019.
+//  Copyright © 2019 Stanislav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
